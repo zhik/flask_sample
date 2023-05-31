872a2c51345d40d39855da8747fae3260fb35aba
Zhi Keng He <zhikhe@gmail.com>
2023-05-31 17:01:24 -0400

    incomplete code

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,47 +1,108 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import * as d3 from "d3";
+  import * as turf from "@turf/turf";
+  import { validate_store } from "svelte/internal";
+
+  let date = "2023-05-13";
+  let start_time = "08:30";
+  let end_time = "10:30";
+  let div;
+
+  function submit(event) {
+    const formData = new FormData(event.target);
+
+    const url = `http://localhost:5000/speed?date=${formData.get(
+      "date"
+    )}&start=${formData.get("start_time")}&end=${formData.get("end_time")}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        const group = d3.group(
+          data,
+          (d) => d.trip_id + ":" + new Date(d.timestamp).getHours()
+        );
+        const grouped = Array.from(group, ([key, value]) => {
+          const max = d3.max(value, (d) => new Date(d.timestamp));
+          const min = d3.min(value, (d) => new Date(d.timestamp));
+          const seconds = (max - min) / 1000;
+
+          let line = null;
+          if (value.length > 2) {
+            line = turf.lineString(
+              value.map((d) => [d.longitude, d.latitude]),
+              { name: "line 1" }
+            );
+            const length = turf.length(line, { units: "miles" });
+            const mph = length / (seconds / 3600);
+            return { length, mph, hour: key.split(":")[1] };
+          }
+          return { seconds };
+        });
+
+        const avg_per_hour = Array.from(
+          d3.group(grouped, (d) => d.hour),
+          ([key, value]) => {
+            return { hour: key, avg: d3.mean(value, (d) => d.mph) };
+          }
+        )
+          .filter((d) => d.hour)
+          .sort((a, b) => Number(a.hour) > Number(b.hour));
+
+
+        const margin = {
+          top: 20,
+          bottom: 0,
+          left: 15,
+          right: 50,
+        };
+
+        const w = div.getBoundingClientRect().width;
+
+        const bar_width = w / 24;
+
+        const h = 500;
+        console.log(div)
+        const svg = d3
+          .select(div)
+          .append("svg")
+          .attr("width", w)
+          .attr("height", h)
+          .attr("viewBox", [0, 0, w, h])
+          .attr("style", "max-width: 100%; height: intrinsic;");
+        const bars = svg
+          .append("g")
+          .attr(
+            "transform",
+            `translate(${margin.left}, ${margin.top}) scale(1)`
+          );
+
+        const yScale = d3
+          .scaleLinear()
+          .domain(d3.extent(avg_per_hour, (d) => d.avg)) // unit: km
+          .range([0, h - (margin.bottom + margin.top)]); // unit: pixels
+
+        bars
+          .selectAll("rect")
+          .data(avg_per_hour)
+          .enter()
+          .append("rect")
+          .attr("x", (d, i) => bar_width * Number(d.hour))
+          .attr("y", (d) => yScale(d.avg))
+          .attr("height", (d) => {
+            console.log(h, d)
+            return h - yScale(d.avg)
+          })
+          .attr("width", bar_width)
+          .attr("fill", "#69b3a2");
+      });
+  }
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<form action="" on:submit|preventDefault={submit}>
+  <input type="date" name="date" bind:value={date} />
+  <input type="time" name="start_time" bind:value={start_time} />
+  <input type="time" name="end_time" bind:value={end_time} />
+  <button type="submit">Query data</button>
+</form>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<div bind:this={div} class="chart w-full h-64" />
--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, send_file, render_template, jsonify
+from flask import Flask, send_file, render_template, jsonify, request
+from flask_cors import CORS
 import pandas as pd
 import os
 import psycopg2
@@ -7,11 +8,32 @@ from db_setup import setup
 
 engine = sa.create_engine('postgresql://docker:docker@postgres:5432/default')
 app = Flask(__name__, template_folder='templates')
+CORS(app)
 
 @app.route('/')
 def home():
     rows = pd.read_sql("select count(*) from bus", engine).iloc[0]['count']
     return render_template('index.html', rows = rows)
+
+@app.route('/speed')
+def getSpeed():
+    route_id = request.args.get('route_id')
+    if not route_id:
+        route_id = 'B41'
+
+    date = request.args.get('date')
+    start_time = request.args.get('start')
+    end_time = request.args.get('end')
+
+    bus_positions_B41 = pd.read_sql(f"""
+        select timestamp, trip_id, longitude, latitude from bus 
+        where route_id = '{route_id}' 
+    """, engine)
+
+    bus_positions_B41['timestamp'] = pd.to_datetime(bus_positions_B41['timestamp'])
+
+
+    return jsonify(bus_positions_B41.to_dict(orient = 'records'))
 
 if __name__ == '__main__':
     setup(engine)
